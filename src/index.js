1d72cc959092422e0376514c04094877f165c440
Dhruv Gopani <dhruvgopani8@gmail.com>
2023-05-09 20:51:56 +0530

    'frontend'

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import store from './store';
+import AlertTemplate from 'react-alert-template-basic'
+import { positions, transitions, Provider as AlertProvider } from 'react-alert';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const option = {
+  timeout: 5000,
+  postion: positions.BOTTOM_CENTER,
+  transition: transitions.SCALE
+}
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <AlertProvider template={AlertTemplate} {...option} >
+        <App />
+      </AlertProvider>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
